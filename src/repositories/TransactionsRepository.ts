faab7891792709e6b358f9b2842cfcf260f7d1b0
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2020-04-22 00:46:40 -0300

    Challege Finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -14,15 +15,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accumulated, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulated.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulated.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulated;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    const newTransaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(newTransaction);
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    // validation
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Transaction type Invalid, try again');
+    }
+    if (value === 0 || typeof value !== 'number') {
+      throw Error('Transaction Value must be a number higher than zero');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('You dont have enought balance for this operation');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
